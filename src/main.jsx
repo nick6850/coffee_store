4187ffc7a4376f21fcf65fbecd202cc1b528aeac
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-02-26 21:21:37 +0300

    Wrapped the App component in context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
+import StoresProvider from "./contexts/storesContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <StoresProvider>
+      <App />
+    </StoresProvider>
   </React.StrictMode>
 );
