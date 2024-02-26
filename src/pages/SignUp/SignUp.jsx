7808a1c83e212aa7ef4692fb0039eb66482f80b3
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-02-26 21:22:37 +0300

    Implemented sign-up page logic

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Alert } from "antd";
+import { observer } from "mobx-react";
+import styles from "./SignUp.module.scss";
+import { Navigate } from "react-router";
+import { Link } from "react-router-dom";
+import { useStores } from "../../contexts/storesContext";
+
+const SignUp = observer(() => {
+  const { authStore } = useStores();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onFinish = (values) => {
+    try {
+      authStore.signUp(values.username, values.password);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  if (authStore.isSignedIn) {
+    return <Navigate to="/" />;
+  }
+
+  const onFinishFailed = (errorInfo) => {
+    setErrorMessage(errorInfo);
+  };
+
+  return (
+    <div className={styles.formContainer}>
+      <Form
+        className={styles.form}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <h1 className={styles.title}>Регистрация</h1>
+        <Form.Item
+          label="Логин"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите ваш логин!",
+            },
+            {
+              min: 3,
+              message: "Имя пользователя должно быть не менее 3 символов",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Пароль"
+          name="password"
+          rules={[
+            { required: true, message: "Пожалуйста, введите ваш пароль!" },
+            { min: 3, message: "Пароль должен быть не менее 3 символов" },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Отправить
+          </Button>
+        </Form.Item>
+
+        {errorMessage && (
+          <Alert
+            message="Ошибка"
+            description={errorMessage}
+            type="error"
+            showIcon
+          />
+        )}
+        <p style={{ textAlign: "center" }}>
+          Уже есть аккаунт? <Link to="/login">Войдите здесь</Link>
+        </p>
+      </Form>
+    </div>
+  );
+});
+
+export default SignUp;
