cd40aee19f121f97122f7e1908db8da41f34261b
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-02-27 00:37:17 +0300

    Implemented login page logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Input, Button, Alert } from "antd";
+import { observer } from "mobx-react";
+import { useStores } from "../../contexts/storesContext";
+import { Link, Navigate } from "react-router-dom";
+import styles from "./Login.module.scss";
 
-function Login() {
-  return <div>Login</div>;
-}
+const Login = observer(() => {
+  const { authStore } = useStores();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  if (authStore.isSignedIn) {
+    return <Navigate to="/" />;
+  }
+
+  const onFinish = (values) => {
+    try {
+      authStore.signIn(values.username, values.password);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <div className={styles.formContainer}>
+      <Form
+        name="login"
+        className={styles.loginForm}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите ваше имя пользователя!",
+            },
+          ]}
+        >
+          <Input placeholder="Имя пользователя" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            { required: true, message: "Пожалуйста, введите ваш пароль!" },
+          ]}
+        >
+          <Input.Password placeholder="Пароль" />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className={styles.loginButton}
+          >
+            Войти
+          </Button>
+        </Form.Item>
+        {errorMessage && (
+          <Alert
+            message="Ошибка"
+            description={errorMessage}
+            type="error"
+            showIcon
+          />
+        )}
+        <p className={styles.registerLink}>
+          Нет аккаунта? Зарегистрируйтесь <Link to="/signup">здесь</Link>.
+        </p>
+      </Form>
+    </div>
+  );
+});
 
 export default Login;
