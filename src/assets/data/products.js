3c6dd918f8b939e8af10bc307001da95e9a99921
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-02-27 21:21:03 +0300

    Added products data and photos

---
+++ src/assets/data/products.js
@@ -0,0 +1,79 @@
+const products = [
+  {
+    id: 1,
+    name: "Arabica Blend",
+    description:
+      "Богатые и карамельные ноты, идеально подходят для приготовления эспрессо.",
+    price: 15.99,
+    category: "Зерна",
+    img: "product_1.jpg",
+  },
+  {
+    id: 2,
+    name: "Robusta Blend",
+    description: "Экзотические зерна с уникальными вкусами.",
+    price: 18.99,
+    category: "Зерна",
+    img: "product_2.jpg",
+  },
+  {
+    id: 3,
+    name: "Coffee Blend",
+    description: "Сбалансированная смесь из лучших зерен.",
+    price: 14.99,
+    category: "Зерна",
+    img: "product_3.jpg",
+  },
+  {
+    id: 4,
+    name: "Coffee Machine",
+    description:
+      "Простая в использовании кофеварка для ежедневного приготовления.",
+    price: 120.0,
+    category: "Кофемашины",
+    img: "product_4.jpg",
+  },
+  {
+    id: 5,
+    name: "Espresso Machine",
+    description:
+      "Профессиональная кофемашина для истинных эспрессо-энтузиастов.",
+    price: 599.99,
+    category: "Кофемашины",
+    img: "product_5.jpg",
+  },
+  {
+    id: 6,
+    name: "Portable Maker",
+    description: "Приготовьте свой кофе везде, где вы находитесь.",
+    price: 50.0,
+    category: "Кофемашины",
+    img: "product_6.jpg",
+  },
+  {
+    id: 7,
+    name: "Coffee Grinder",
+    description: "Добейтесь идеального помола для вашего кофе.",
+    price: 75.0,
+    category: "Другое",
+    img: "product_7.jpg",
+  },
+  {
+    id: 8,
+    name: "Milk Frother",
+    description: "Создайте богатую и кремовую пену для вашего кофе.",
+    price: 25.0,
+    category: "Другое",
+    img: "product_8.jpg",
+  },
+  {
+    id: 9,
+    name: "Coffee Mug",
+    description: "Стильная кружка для вашего ежедневного кофе.",
+    price: 10.0,
+    category: "Другое",
+    img: "product_9.jpg",
+  },
+];
+
+export default products;
