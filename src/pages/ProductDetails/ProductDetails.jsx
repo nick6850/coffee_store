6de592a3b912294d845a730f4a013bda877b1e12
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-02-27 21:41:20 +0300

    Created responsive product details page

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Card, Button, message, Spin } from "antd";
+import { observer } from "mobx-react";
+import NotFound from "../NotFound/NotFound";
+import { useStores } from "../../contexts/storesContext";
+import styles from "./ProductDetails.module.scss";
 
-function ProductDetails() {
-  return <div>ProductDetails</div>;
-}
+const ProductDetails = observer(() => {
+  const { productStore, basketStore } = useStores();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [notFound, setNotFound] = useState(false);
+
+  useEffect(() => {
+    const productDetails = productStore.findById(parseInt(id));
+    if (!productDetails) {
+      setNotFound(true);
+    } else {
+      setProduct(productDetails);
+      setNotFound(false);
+    }
+    setLoading(false);
+  }, [id, productStore]);
+
+  if (loading) {
+    return (
+      <div className={styles.productDetails}>
+        <Spin tip="Загрузка..." />
+      </div>
+    );
+  }
+
+  if (notFound) {
+    return <NotFound />;
+  }
+
+  const handleAddToBasket = () => {
+    basketStore.addProduct(product);
+    message.success(`${product.name} уже в твоей корзине!`, 5);
+  };
+
+  return (
+    <div className={styles.productDetails}>
+      <Card
+        hoverable
+        className={styles.productCard}
+        cover={
+          <img alt={product.name} src={`src/assets/images/${product.img}`} />
+        }
+      >
+        <Card.Meta
+          title={product.name}
+          description={
+            <div className={styles.description}>{product.description}</div>
+          }
+        />
+        <div className={styles.otherInfo}>
+          <p>
+            Категория:
+            <span style={{ fontWeight: "bold" }}>{product.category}</span>
+          </p>
+          <p>
+            Цена: <span style={{ fontWeight: "bold" }}>${product.price}</span>
+          </p>
+          <div className={styles.actions}>
+            <Button onClick={() => navigate(-1)}>К списку товаров</Button>
+            <Button type="primary" onClick={handleAddToBasket}>
+              Добавить в корзину
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+});
 
 export default ProductDetails;
